3b364fb27707144c29f133e7a38d1b9a8849da15
admin <dely>
2023-02-12 14:39:55 +0800

    add vc-form

--- js/vue/vc-form.js
+++ js/vue/vc-form.js
@@ -1,10 +1,15 @@
-Vue.component('vc-form', {
+export default {
   template: `<form>
-<label v-for="(item, index) in formItems">{{item.name || item.prop}}
-    <input v-if="item.type === 'number'" type="number" v-model="formData[item.prop]" @change="(val) => inputChange(val, item)"/>
-    <input v-else  v-model="formData[item.prop]" @change="(val) => inputChange(val, item)"/>
-</label>
+  <template v-for="(item, index) in formItems">
+
+    <label>{{item.name || item.prop}}
+      <input v-if="item.type === 'number'" type="number" v-model="formData[item.prop]" @change="(val) => inputChange(val, item)"/>
+      <input v-else  v-model="formData[item.prop]" @change="(val) => inputChange(val, item)"/>
+    </label>
+    <br/>
+  </template>
 </form>`,
+  name: 'vc-form',
   props: {
     formItems: {
       type: Array,
@@ -23,14 +28,24 @@ Vue.component('vc-form', {
     }
   },
   data() {
+    const cache = JSON.parse(localStorage.getItem(this.storageKey) || '{}');
     return {
-      formData: {...this.modelValue},
+      formData: {...this.modelValue, ...cache},
+    }
+  },
+  beforeMount() {
+    const cache = JSON.parse(localStorage.getItem(this.storageKey) || 'null');
+    if (cache != null) {
+      this.$emit('update:modelValue', this.formData)
     }
   },
   methods: {
     inputChange(val, item) {
-      this.$emits('update:modelValue')
-      this.$emits('change', item.name, val, item)
+      this.$emit('update:modelValue', this.formData)
+      this.$emit('change', item.name, val, item)
+      if (this.storageKey) {
+        localStorage.setItem(this.storageKey, JSON.stringify(this.formData))
+      }
     }
   }
-})
+}
